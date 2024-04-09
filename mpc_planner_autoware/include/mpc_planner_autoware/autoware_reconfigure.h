8983e435a6adcf0624c78592dec62b6eeac1e181
oscardegroot <groot.oscar.de@gmail.com>
2024-04-09 15:44:13 +0200

    add autoware generated solver files to compile in CI.

---
+++ mpc_planner_autoware/include/mpc_planner_autoware/autoware_reconfigure.h
@@ -0,0 +1,55 @@
+#ifndef __GENERATED_RECONFIGURE_H
+#define __GENERATED_RECONFIGURE_H
+
+#include <ros/ros.h>
+
+#include <ros_tools/logging.h>
+#include <mpc_planner_util/parameters.h>
+
+// Dynamic Reconfigure server
+#include <dynamic_reconfigure/server.h>
+#include <mpc_planner_autoware/Config.h>
+
+class AutowareReconfigure
+{
+public:
+	AutowareReconfigure()
+	{
+		// Initialize the dynamic reconfiguration
+		LOG_INFO("Setting up dynamic_reconfigure server for the parameters");
+		// first_reconfigure_callback_ = true;
+		ros::NodeHandle nh_reconfigure("mpc_planner_autoware");
+		_reconfigure_server.reset(new dynamic_reconfigure::Server<mpc_planner_autoware::Config>(_reconfig_mutex, nh_reconfigure));
+		_reconfigure_server->setCallback(boost::bind(&AutowareReconfigure::reconfigureCallback, this, _1, _2));
+	}
+	void reconfigureCallback(mpc_planner_autoware::Config &config, uint32_t level)
+	{
+		(void)level;
+		if (_first_reconfigure_callback){
+			config.acceleration = CONFIG["weights"]["acceleration"].as<double>();
+			config.angular_velocity = CONFIG["weights"]["angular_velocity"].as<double>();
+			config.velocity = CONFIG["weights"]["velocity"].as<double>();
+			config.reference_velocity = CONFIG["weights"]["reference_velocity"].as<double>();
+			config.contour = CONFIG["weights"]["contour"].as<double>();
+			config.lag = CONFIG["weights"]["lag"].as<double>();
+			config.preview = CONFIG["weights"]["preview"].as<double>();
+			_first_reconfigure_callback = false;
+		}else{
+			CONFIG["weights"]["acceleration"] = config.acceleration;
+			CONFIG["weights"]["angular_velocity"] = config.angular_velocity;
+			CONFIG["weights"]["velocity"] = config.velocity;
+			CONFIG["weights"]["reference_velocity"] = config.reference_velocity;
+			CONFIG["weights"]["contour"] = config.contour;
+			CONFIG["weights"]["lag"] = config.lag;
+			CONFIG["weights"]["preview"] = config.preview;
+		}
+	}
+
+private:
+	bool _first_reconfigure_callback{true};
+	// RQT Reconfigure ROS1
+	boost::shared_ptr<dynamic_reconfigure::Server<mpc_planner_autoware::Config>> _reconfigure_server;
+	boost::recursive_mutex _reconfig_mutex;
+};
+
+#endif // __GENERATED_RECONFIGURE_H
---
+++ mpc_planner_autoware/include/mpc_planner_autoware/autoware_ros2_reconfigure.h
@@ -0,0 +1,70 @@
+#ifndef __GENERATED_ROS2_RECONFIGURE_H
+#define __GENERATED_ROS2_RECONFIGURE_H
+#include <rclcpp/rclcpp.hpp>
+#include <ros_tools/logging.h>
+#include <ros_tools/ros2_wrappers.h>
+#include <mpc_planner_util/parameters.h>
+template <class T>
+bool updateParam(const std::vector<rclcpp::Parameter> &params, const std::string &name, YAML::Node value)
+{
+	const auto itr = std::find_if(
+		params.cbegin(), params.cend(),
+		[&name](const rclcpp::Parameter &p)
+		{ return p.get_name() == name; });
+
+	// Not found
+	if (itr == params.cend())
+	{
+		return false;
+	}
+
+	value = itr->template get_value<T>();
+	LOG_INFO("Parameter " + name + " set to " + std::to_string(value.as<T>()));
+	return true;
+}
+
+class AutowareReconfigure
+{
+public:
+	AutowareReconfigure(rclcpp::Node *node)
+	{
+		LOG_INFO("Setting up dynamic_reconfigure parameters");
+
+		declareROSParameters(node);
+
+		_set_param_res = node->add_on_set_parameters_callback(
+			std::bind(&AutowareReconfigure::updateROSParameters, this, std::placeholders::_1));
+	}
+
+	virtual void declareROSParameters(rclcpp::Node *node)
+	{
+		node->declare_parameter<double>("acceleration", CONFIG["weights"]["acceleration"].as<double>());
+		node->declare_parameter<double>("angular_velocity", CONFIG["weights"]["angular_velocity"].as<double>());
+		node->declare_parameter<double>("velocity", CONFIG["weights"]["velocity"].as<double>());
+		node->declare_parameter<double>("reference_velocity", CONFIG["weights"]["reference_velocity"].as<double>());
+		node->declare_parameter<double>("contour", CONFIG["weights"]["contour"].as<double>());
+		node->declare_parameter<double>("lag", CONFIG["weights"]["lag"].as<double>());
+		node->declare_parameter<double>("preview", CONFIG["weights"]["preview"].as<double>());
+	}
+
+	virtual rcl_interfaces::msg::SetParametersResult updateROSParameters(const std::vector<rclcpp::Parameter> &parameters)
+	{
+		updateParam<double>(parameters, "acceleration", CONFIG["weights"]["acceleration"]);
+		updateParam<double>(parameters, "angular_velocity", CONFIG["weights"]["angular_velocity"]);
+		updateParam<double>(parameters, "velocity", CONFIG["weights"]["velocity"]);
+		updateParam<double>(parameters, "reference_velocity", CONFIG["weights"]["reference_velocity"]);
+		updateParam<double>(parameters, "contour", CONFIG["weights"]["contour"]);
+		updateParam<double>(parameters, "lag", CONFIG["weights"]["lag"]);
+		updateParam<double>(parameters, "preview", CONFIG["weights"]["preview"]);
+
+		auto result = rcl_interfaces::msg::SetParametersResult();
+		result.successful = true;
+
+
+		return result;
+	}
+
+protected:
+	rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr _set_param_res;
+};
+#endif // __GENERATED_ROS2_RECONFIGURE_H
